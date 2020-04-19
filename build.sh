7be4ecc8d31f7795d8f39387a8fd54f11d64f73d
paule32 <kallup.jens@web.de>
2020-04-19 14:17:42 +0200

    update

--- build.sh
+++ build.sh
@@ -13,7 +13,4 @@ g++-5 -m32 -shared -o libwrap32ae.so \
 g++-5 -m32 -o test1 wrap32me.cc -L. -l wrap32ae
 strip test1
 
-SAVE_PATH=$LD_LIBRARY_PATH
-export LD_LIBRARY_PARG=.;./test2
-
-LD_LIBRARY_PATH=${SAVE_PATH}
+export LD_LIBRARY_PATH=.;./test2
